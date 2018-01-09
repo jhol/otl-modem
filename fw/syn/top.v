fc0422088fb1073d93700792e329c541ee575d97
Joel Holdsworth <joel@airwebreathe.org.uk>
2018-01-08 21:53:25 -0700

    Added initial HDL build

---
+++ fw/syn/top.v
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2018 Joel Holdsworth <joel@airwebreathe.org.uk>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+module top(clk_100mhz);
+parameter ClkFreq = 50000000; // Hz
+
+input clk_100mhz;
+
+// Clock Generator
+wire clk_50mhz;
+wire pll_locked;
+
+SB_PLL40_PAD #(
+  .FEEDBACK_PATH("SIMPLE"),
+  .DELAY_ADJUSTMENT_MODE_FEEDBACK("FIXED"),
+  .DELAY_ADJUSTMENT_MODE_RELATIVE("FIXED"),
+  .PLLOUT_SELECT("GENCLK"),
+  .FDA_FEEDBACK(4'b1111),
+  .FDA_RELATIVE(4'b1111),
+  .DIVR(4'b0000),
+  .DIVF(7'b0000111),
+  .DIVQ(3'b100),
+  .FILTER_RANGE(3'b101)
+) pll (
+  .PACKAGEPIN(clk_100mhz),
+  .PLLOUTGLOBAL(clk_50mhz),
+  .LOCK(pll_locked),
+  .BYPASS(1'b0),
+  .RESETB(1'b1)
+);
+
+wire clk = clk_50mhz;
+
+// Reset Generator
+reg [3:0] resetn_gen = 0;
+reg reset;
+
+always @(posedge clk) begin
+  reset <= !&resetn_gen;
+  resetn_gen <= {resetn_gen, pll_locked};
+end
+
+endmodule
