d619e065f3aa7e0587d45655bf553c0f009dc56d
Joel Holdsworth <joel@airwebreathe.org.uk>
2018-01-31 22:17:09 -0700

    soapysdr-driver: Implemented initial probing support

--- soapysdr-driver/otl-modem-support.cpp
+++ soapysdr-driver/otl-modem-support.cpp
@@ -17,8 +17,14 @@
  * along with this program; if not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <iostream>
+#include <iomanip>
+#include <memory>
+
+#include <ftdi.h>
 #include <SoapySDR/Device.hpp>
 #include <SoapySDR/Registry.hpp>
+#include <usb.h>
 
 class OTLModem : public SoapySDR::Device {
 public:
@@ -28,9 +34,57 @@ public:
 OTLModem::OTLModem() {
 }
 
+static SoapySDR::KwargsList listDevices() {
+    ftdi_device_list *devlist = nullptr;
+    const std::shared_ptr<ftdi_context> ftdic(ftdi_new(), ftdi_free);
+
+    const int ret = ftdi_usb_find_all(ftdic.get(), &devlist, 0x0403, 0x6010);
+    if (ret < 0) {
+        std::cerr << "Failed to list devices (" << ret << "): " <<
+               ftdi_get_error_string(ftdic.get()) << std::endl;
+        return SoapySDR::KwargsList();
+    }
+
+    SoapySDR::KwargsList devices;
+    for (ftdi_device_list *curdev = devlist; curdev;
+        curdev = curdev->next) {
+        SoapySDR::Kwargs info;
+
+        {
+            std::ostringstream ss;
+            ss << std::setw(3) << std::setfill('0') <<
+                curdev->dev->bus->location;
+            info["busnum"] = ss.str();
+        }
+        {
+            std::ostringstream ss;
+            ss << std::setw(3) << std::setfill('0') <<
+                int(curdev->dev->devnum);
+            info["devnum"] = ss.str();
+        }
+
+        info["filename"] = curdev->dev->filename;
+
+        devices.push_back(info);
+    }
+
+    ftdi_list_free(&devlist);
+
+    return devices;
+}
+
 SoapySDR::KwargsList findOTLModem(const SoapySDR::Kwargs &args) {
-    (void)args;
-    return SoapySDR::KwargsList();
+    const SoapySDR::KwargsList devices = listDevices();
+    SoapySDR::KwargsList results;
+
+    for (SoapySDR::Kwargs device : devices) {
+        if (!(args.count("busnum") && args.at("busnum") != device.at("busnum")) &&
+            !(args.count("devnum") && args.at("devnum") != device.at("devnum")) &&
+            !(args.count("filename") && args.at("filename") != device.at("filename")))
+            results.push_back(device);
+    }
+
+    return results;
 }
 
 SoapySDR::Device *makeOTLModem(const SoapySDR::Kwargs &args) {
